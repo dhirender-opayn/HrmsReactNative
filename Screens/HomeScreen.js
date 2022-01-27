0157edc2babc6669e1dfb7ec9da86479dfcbc2c3
asus <dhirender@opayn.com>
2022-01-27 09:59:45 +0530

    Morning Messgae

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,13 +1,66 @@
 import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
-import { Card } from "../components/Card";
+import { ActivityIndicator, FlatList, Image, SafeAreaView, Text, View } from "react-native";
+import { OtpVerify } from "./OtpVerify";
+import { OverlayContainer } from "../Common/OverlayContainer";
+import AppBackgorund from "./BackgroundView";
+import String, { strings } from "../Common/String";
+import ImagesPath from "../images/ImagesPath";
+import { userwhiteUrl } from "../images/ImagesPath";
+import { CustomStyling } from "../CustomStyle/CustomStyling";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from "react";
+import { AuthStyle } from "../CustomStyle/AuthStyle";
+
+
+
+const HomeScreen = ({ navigation = useNavigation() }) => {
+   const [data, setData] = useState({});
+   const [isLoad, setLoad] = useState(true)
+   var detail = "";
+
+   const retrieveData = async () => {
+      try {
+         detail = await AsyncStorage.getItem('userData');
+         console.log(detail);
+         setData(JSON.parse(detail));
+         console.log("=====>  ", data);
+      }
+      catch (error) {
+         console.error(error);
+      }
+      finally {
+         setLoad(false);
+      }
+   };
+   useEffect(() => {
+      retrieveData();
+   }, []);
+
+   return (
+
+      <OverlayContainer>
+         <AppBackgorund />
+         <View>
+            <Text style={{ marginBottom: 30, fontSize: 30, color: "#fff", textAlign: 'center' }}>{strings.home}</Text>
+            <Image style={CustomStyling.imageThumb} source={require('../images/userwhite.png')} />
+            <View style={{ marginTop: 40 }}>
+               {
+                  isLoad ? <ActivityIndicator /> : (<Text style={CustomStyling.title} >{data.user.name}</Text>)
+               }
+            </View>
+            <View style={{ marginTop: 10 }}>
+               {
+                  isLoad ? <ActivityIndicator /> : (<Text style={CustomStyling.subTitle} >{data.user.name }</Text>)
+               }
+            </View>
+
+            <Text>Hello Dummy</Text>
  
 
-export  const HomeScreen = () => {
-    return(
-        <View>
-            <Card/>
-       </View>
-    ); 
+         </View>
+      </OverlayContainer>
 
- }
+   );
+
+};
+export default HomeScreen;
